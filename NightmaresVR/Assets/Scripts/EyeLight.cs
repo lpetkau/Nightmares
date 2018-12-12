7b9d2f2b1344a974b842fd4002faac4912c57e89
Lynk Petkau <Lynker-man@hotmail.com>
2018-12-12 13:19:03 -0400

    Made Enemy assets and effects

---
+++ NightmaresVR/Assets/Scripts/EyeLight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeLight : MonoBehaviour {
+
+    
+    public float glowMultiplier;
+
+    private Vector3 target;
+    private GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        target = player.transform.position;
+        GetComponent<Light>().intensity = (Vector3.Distance(target, this.transform.position) * glowMultiplier);
+	}
+}
