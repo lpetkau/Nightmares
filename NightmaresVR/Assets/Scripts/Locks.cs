6b68f6fbea6d494e8004f0866f7a640b65f16a1a
MrSleepen <43214874+MrSleepen@users.noreply.github.com>
2018-12-13 22:24:18 -0400

    Merge 2 Not Final Waiting on lynk and meg

--- NightmaresVR/Assets/Scripts/Locks.cs
+++ NightmaresVR/Assets/Scripts/Locks.cs
@@ -7,22 +7,36 @@ public class Locks : MonoBehaviour {
 
 
     public GameObject Key;
-   
+    public int DoorNumber;
 
     public void OnTriggerEnter(Collider collider)
     {
         switch (collider.gameObject.tag)
         {
             case "Grab":
-
-                if (collider.gameObject.name == "Key1")
+                // Door1
+                if (collider.gameObject.name == "Key1"  &&  DoorNumber == 1)
                 {
                     GameManager.Instance.Door1Locked = false;
+                    Debug.Log("Unlock");
                 }
 
-                else if(collider.gameObject.name == "Key2")
+                else if(collider.gameObject.name == "Key2" && DoorNumber == 1)
                 {
-                    GameManager.Instance.Door2Locked = false;  
+                    GameManager.Instance.Door2Locked = false;
+                    Debug.Log("wrong Key");
+                }
+                //Door2
+                if (collider.gameObject.name == "Key1" && DoorNumber == 2)
+                {
+                    GameManager.Instance.Door1Locked = false;
+                    Debug.Log("wrong Key");
+                }
+
+                else if (collider.gameObject.name == "Key2" && DoorNumber == 2)
+                {
+                    GameManager.Instance.Door2Locked = false;
+                    Debug.Log("Unlock");
                 }
 
                 break;
