8f2c1a679e623a8e8f1600f39019a83c5542cc9c
DESKTOP-348KFFI\morga <morgan.webber@easternstudent.ca>
2018-12-28 13:49:32 -0400

    working on PC Grabing

--- NightmaresVR/Assets/Scripts/PickupObject.cs
+++ NightmaresVR/Assets/Scripts/PickupObject.cs
@@ -18,7 +18,7 @@ public class PickupObject : MonoBehaviour {
 		
 	}
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerStay(Collider other)
     {
         //if (Input.GetButton("Pickup"))
         //{
--- NightmaresVR/Assets/Scripts/PlayerInteractions.cs
+++ NightmaresVR/Assets/Scripts/PlayerInteractions.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerInteractions : MonoBehaviour {
 
+    public Vector3 direction;
+    public RaycastHit hit;
+    public float maxDistance = 100;
+    public LayerMask layermask;
+
+
+
     public bool Pickup = false;
 
 	// Use this for initialization
@@ -19,18 +26,79 @@ public class PlayerInteractions : MonoBehaviour {
             // Swap Pickup true/false
             if(Pickup == false)
             {
-                Debug.Log("Pickup TRUE");
+                //Debug.Log("Pickup TRUE");
                 Pickup = true;
             }
             else
             {
-                Debug.Log("Pickup FALSE");
+                //Debug.Log("Pickup FALSE");
                 Pickup = false;
             }
 
 
             // do raycast
             // if raycast fails, Pickup = false
+
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, maxDistance, layermask)) // Camera.main.transform.forward
+            {
+
+                if (hit.transform.gameObject.tag != "MainCamera" && hit.transform.gameObject.tag != "Player" && hit.transform.gameObject.tag != "LeftHand" && hit.transform.gameObject.tag != "RightHand")
+                {
+                    Debug.Log("Hit");
+                    Debug.Log(hit.distance);
+                    Debug.Log(hit.transform.gameObject.tag);
+                    Debug.Log(hit.transform.name);
+                }
+                else
+                {
+                    Debug.Log("Hit Player Component");
+                }
+
+            }
+
+
+            //int layerMask = 10; // Layer = Grabables
+            //RaycastHit hit;
+
+
+            // Does the ray intersect any objects from the Grabables Layer
+
+            //if (Physics.Raycast(transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity, layerMask)) // QueryTriggerInteraction.UseGlobal
+            //{
+            //    if(hit.transform.gameObject.tag != "MainCamera" && hit.transform.gameObject.tag != "Player" && hit.transform.gameObject.tag != "LeftHand" && hit.transform.gameObject.tag != "RightHand")
+            //    {
+            //        Debug.Log("Hit");
+            //        Debug.DrawRay(transform.position, Camera.main.transform.forward, Color.yellow);
+            //        Debug.Log(hit.distance);
+            //        Debug.Log(hit.transform.gameObject.tag);
+            //        //Destroy(hit.transform.gameObject);
+            //    }
+            //    else
+            //    {
+            //        Debug.Log("Hit Player Component");
+            //    }
+
+            //}
+            //else
+            //{
+            //    //Debug.DrawRay(transform.position, Camera.main.transform.forward * 1000, Color.white);
+            //    Debug.Log("Did not Hit");
+            //    Debug.Log(hit.distance);
+            //}
+
+
+
+            //Physics.Raycast(transform.position, Camera.main.transform.forward, 0, 1000, 0);
+
+            //Vector3 fwd = transform.TransformDirection(Vector3.forward);
+            //Vector3 Ray = transform.TransformDirection(Camera.main.transform.forward);
+
+            //if (Physics.Raycast(transform.position, Ray, 2))
+            //{
+            //    print("There is something in front of the object!");
+            //}
+
+
         }
 
 
@@ -39,7 +107,8 @@ public class PlayerInteractions : MonoBehaviour {
 
     void GrabItem()
     {
-
+        GameObject Crowbar = GameObject.Find("Crowbar");
+        Crowbar.transform.position = transform.position;
     }
 
 }
