a119ec75d6b957c1dbfc45a1b7c19ef4ac10632b
DESKTOP-348KFFI\morga <morgan.webber@easternstudent.ca>
2018-12-31 18:21:46 -0400

    BugFix: Items no longer fall off map

---
+++ NightmaresVR/Assets/Scripts/ItemReturn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemReturn : MonoBehaviour {
+
+    private float ItemRotation;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Crowbar" || other.tag == "Grab")
+        {
+            GameObject Player = GameObject.Find("AdvancedPlayer");
+            PlayerInteractions playerScript = Player.GetComponent<PlayerInteractions>();
+            playerScript.Pickup = true; // activate SnapPoint
+            ItemRotation = playerScript.fixedRotation;
+            
+            other.transform.parent.position = Player.transform.GetChild(3).position; // move item to SnapPoint
+            other.transform.parent.eulerAngles = new Vector3(ItemRotation, ItemRotation, ItemRotation);
+            other.transform.parent.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        }
+    }
+
+}
