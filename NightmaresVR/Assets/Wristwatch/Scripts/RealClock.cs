3a1bf26d8b06cbe19b17868a8a38e817cf6a122f
MrSleepen <43214874+MrSleepen@users.noreply.github.com>
2019-01-05 17:09:34 -0400

    Addent mental health and watch to keep track

---
+++ NightmaresVR/Assets/Wristwatch/Scripts/RealClock.cs
@@ -0,0 +1,28 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class RealClock : MonoBehaviour {
+	public Transform Hours;
+	
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+		float hour = GameManager.Instance.MentalHealth;
+		
+
+		
+
+
+		if(Hours)
+			Hours.localRotation = Quaternion.Euler (0, 0, hour / 100 * 360);
+
+	
+
+
+	}
+}
