f775753af9a7af882400415d966057cf3ca5ec3d
MrSleepen <43214874+MrSleepen@users.noreply.github.com>
2019-01-05 14:49:31 -0400

    Added Flickering lights in bathroom with proximity switch

---
+++ NightmaresVR/Assets/Scripts/BRlightsoff.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BRlightsoff : MonoBehaviour {
+    public GameObject Flashinglights;
+
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        switch (collider.gameObject.name)
+        {
+            case "AdvancedPlayer":
+                Flashinglights.SetActive(false);
+                break;
+        }
+    }
+}
