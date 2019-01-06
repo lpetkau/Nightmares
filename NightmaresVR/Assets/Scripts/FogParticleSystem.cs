883fc6e85d7cdab34bf187db377d2db4de77f3f7
Mistyxo <meagan.sleep@easternstudent.ca>
2019-01-06 14:12:04 -0400

    hint system without polish and bathrooms triggers may be buggy

---
+++ NightmaresVR/Assets/Scripts/FogParticleSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogParticleSystem : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponentInChildren<ParticleSystem>().Play();
+        }
+    }
+}
+
+    
---
+++ NightmaresVR/Assets/Scripts/TapTriggers.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapTriggers : MonoBehaviour
+{
+
+    public Transform Spawnpoint;
+    public Rigidbody Prefab;
+    public GameObject uiObject;
+
+    void Start()
+    {
+
+        uiObject.SetActive(false);
+    }
+
+    void OnTriggerEnter()
+    {
+        Rigidbody RigidPrefab;
+        RigidPrefab = Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation) as Rigidbody;
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponentInChildren<ParticleSystem>().Play();
+        }
+    
+        if (other.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(20);
+        Destroy(uiObject);
+        Destroy(gameObject);
+
+    }
+}
+
+
+
+
+
+
+
+
+
