ca7295197b1a7aebb1974aa2dafd1497bccfb01b
Mistyxo <meagan.sleep@easternstudent.ca>
2019-01-06 11:15:57 -0400

    I hope this works and source tree doesn't crash again

---
+++ NightmaresVR/Assets/Scripts/ScrollScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollScript : MonoBehaviour
+{
+
+    [SerializeField] private Image customImage;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            customImage.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            customImage.enabled = false;
+        }
+    }
+}
+	
+    
+    
+    
+  
