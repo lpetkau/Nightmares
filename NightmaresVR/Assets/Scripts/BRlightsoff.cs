e8b080b58a50c0e676441f3b92407556b49e4478
MrSleepen <43214874+MrSleepen@users.noreply.github.com>
2019-01-09 11:54:44 -0400

    added lighning and door slamming

--- NightmaresVR/Assets/Scripts/BRlightsoff.cs
+++ NightmaresVR/Assets/Scripts/BRlightsoff.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BRlightsoff : MonoBehaviour {
     public GameObject Flashinglights;
+    public GameObject Door;
+    public GameObject Trigger;
 
 
     public void OnTriggerEnter(Collider collider)
@@ -12,6 +14,8 @@ public class BRlightsoff : MonoBehaviour {
         {
             case "AdvancedPlayer":
                 Flashinglights.SetActive(false);
+                Door.transform.rotation = Quaternion.Euler(0, -14.179F, 90);
+                Trigger.SetActive(false);
                 break;
         }
     }
